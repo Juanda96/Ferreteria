a2efd1702248fa5c06d58cd970175b6a6478c2d7
Juanda96 <50851266+Juanda96@users.noreply.github.com>
2020-10-24 07:09:47 -0600

    Create user Entity and make a new layer 'DataAccessObject' to capture the information and save in a object.

---
+++ Ferreteria/Entity/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class User
+    {
+        private int _id;
+        private int _dni;
+        private string _name;
+        private string _last;
+        private string _code;
+        private string _type;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int MyProperty
+        {
+            set { _dni = value; }
+            get { return _dni; }
+        }
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string last
+        {
+            get { return _last; }
+            set { _last = value; }
+        }
+
+        public string  code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public string type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+    }
+}
